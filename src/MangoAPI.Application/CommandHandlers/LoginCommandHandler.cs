14c34134657e0c3e19e714af356f437264176f8f
Petro Kolosov <pkolosov@openskydata.com>
2021-06-25 14:53:43 +0200

    project references modified

--- src/MangoAPI.Application/CommandHandlers/LoginCommandHandler.cs
+++ src/MangoAPI.Application/CommandHandlers/LoginCommandHandler.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using MangoAPI.Application.Exceptions;
 using MangoAPI.Application.Interfaces;
-using MangoAPI.Domain;
 using MangoAPI.Domain.Entities;
 using MangoAPI.DTO.Commands;
 using MangoAPI.DTO.Models;
