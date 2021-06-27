cc27d53f7586ef18a6f92b4bef25daabe3b531e0
Petro Kolosov <pkolosov@openskydata.com>
2021-06-27 12:46:12 +0200

    project refactored

--- MangoAPI.Infrastructure/Validators/JwtSecurityTokenValidator.cs
+++ MangoAPI.Infrastructure/Validators/JwtSecurityTokenValidator.cs
@@ -2,7 +2,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 
 namespace MangoAPI.Infrastructure.Validators
