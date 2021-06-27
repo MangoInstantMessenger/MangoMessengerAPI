2af4d62a715bc498f22e7fc841f8deb82a74b281
Nikolay Dyachenko <aerowind10@gmail.com>
2021-06-27 23:04:19 +0300

    Create LogoutCommand.cs

---
+++ MangoAPI.DTO/Commands/Auth/LogoutCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace MangoAPI.DTO.Commands.Auth
+{
+    public class LogoutCommand
+    {
+        public string RefreshTokenId { get; set; }
+        public string IpAddress { get; set; }
+        public string UserAgent { get; set; }
+        public string FingerPrint { get; set; }
+    }
+}
