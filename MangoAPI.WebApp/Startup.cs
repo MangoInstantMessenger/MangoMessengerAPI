55153fee8b35019e2734b48cacefd5072599e3fb
Petro Kolosov <pkolosov@openskydata.com>
2021-06-27 23:30:37 +0200

    fix: startup class updated

--- MangoAPI.WebApp/Startup.cs
+++ MangoAPI.WebApp/Startup.cs
@@ -85,16 +85,22 @@ namespace MangoAPI.WebApp
 
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MangoAPI v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MangoAPI v1"));
 
+            app.UseHttpsRedirection();
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
