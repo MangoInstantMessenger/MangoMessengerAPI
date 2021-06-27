eb2a419ebf94955a5bddb573b402e0762c911e2a
Nikolay Dyachenko <aerowind10@gmail.com>
2021-06-27 22:28:37 +0300

    Downgrade packages to stable

--- MangoAPI.Infrastructure/Migrations/20210626020008_MessagesEntityCreated.cs
+++ MangoAPI.Infrastructure/Migrations/20210626020008_MessagesEntityCreated.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace MangoAPI.Infrastructure.Migrations
 {
