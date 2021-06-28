a9ee8d886ff7fe75214e4b624bc453f037236c34
Nikolay Dyachenko <aerowind10@gmail.com>
2021-06-28 20:42:59 +0300

    Remove User Field

--- MangoAPI.Domain/Entities/UserEntity.cs
+++ MangoAPI.Domain/Entities/UserEntity.cs
@@ -8,9 +8,7 @@ namespace MangoAPI.Domain.Entities
         public string DisplayName { get; set; }
         public string Image { get; set; }
         public string Bio { get; set; }
-        public bool Verified { get; set; }
         public int ConfirmationCode { get; set; }
-
         public virtual ICollection<RefreshTokenEntity> RefreshTokens { get; set; }
         public virtual ICollection<MessageEntity> Messages { get; set; }
         public virtual ICollection<UserChatEntity> UserChats { get; set; }
