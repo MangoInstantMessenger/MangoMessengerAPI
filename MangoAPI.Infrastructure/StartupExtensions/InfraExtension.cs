4f069d2eb4d4d6beec5f94c7fffe31e2c7b75994
Nikolay Dyachenko <aerowind10@gmail.com>
2021-06-29 16:33:06 +0300

    Startup extensions

---
+++ MangoAPI.Infrastructure/StartupExtensions/InfraExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MangoAPI.Infrastructure.StartupExtensions
+{
+    public static class InfraExtension
+    {
+        public static IServiceCollection AddAppInfrastructure(this IServiceCollection services)
+        {
+            services.AddPostgresDb();
+            services.AddIdentityUsers();
+            services.AddAppServices();
+            services.AddAppAuthorization();
+            services.AddAppAuthentication();
+            return services;
+        }
+    }
+}
