89f9091581f0b45517df9d49841b0408d6b9dbd7
Petro Kolosov <pkolosov@openskydata.com>
2021-07-01 23:45:21 +0200

    redundant endpoint removed

--- MangoAPI.WebApp/Interfaces/IMessagesController.cs
+++ MangoAPI.WebApp/Interfaces/IMessagesController.cs
@@ -1,14 +1,12 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MangoAPI.DTO.Commands.Messages;
-using MangoAPI.DTO.Queries.Messages;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MangoAPI.WebApp.Interfaces
 {
     public interface IMessagesController
     {
-        Task<IActionResult> GetChatMessages(GetMessagesQuery query, CancellationToken cancellationToken);
         Task<IActionResult> SendMessage(SendMessageCommand command, CancellationToken cancellationToken);
         Task<IActionResult> EditMessage(EditMessageCommand command, CancellationToken cancellationToken);
         Task<IActionResult> DeleteMessage(DeleteMessageCommand command, CancellationToken cancellationToken);
