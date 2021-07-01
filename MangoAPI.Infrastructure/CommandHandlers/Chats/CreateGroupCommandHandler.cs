4371378e6bf9d3ee5bb60f9f25b03ff71eb35b54
Petro Kolosov <pkolosov@openskydata.com>
2021-07-01 17:56:22 +0200

    group creation implemented

--- MangoAPI.Infrastructure/CommandHandlers/Chats/CreateGroupCommandHandler.cs
+++ MangoAPI.Infrastructure/CommandHandlers/Chats/CreateGroupCommandHandler.cs
@@ -1,16 +1,84 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using MangoAPI.Application.Services;
+using MangoAPI.Domain.Constants;
+using MangoAPI.Domain.Entities;
+using MangoAPI.Domain.Enums;
 using MangoAPI.DTO.Commands.Chats;
 using MangoAPI.DTO.Responses.Chats;
+using MangoAPI.Infrastructure.Database;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace MangoAPI.Infrastructure.CommandHandlers.Chats
 {
     public class CreateGroupCommandHandler : IRequestHandler<CreateGroupCommand, CreateChatEntityResponse>
     {
-        public async Task<CreateChatEntityResponse> Handle(CreateGroupCommand request, CancellationToken cancellationToken)
+        private readonly ICookieService _cookieService;
+        private readonly IJwtRefreshService _jwtRefreshService;
+        private readonly IRequestMetadataService _metadataService;
+        private readonly MangoPostgresDbContext _postgresDbContext;
+
+        public CreateGroupCommandHandler(ICookieService cookieService, IJwtRefreshService jwtRefreshService,
+            IRequestMetadataService metadataService, MangoPostgresDbContext postgresDbContext)
         {
-            throw new System.NotImplementedException();
+            _cookieService = cookieService;
+            _jwtRefreshService = jwtRefreshService;
+            _metadataService = metadataService;
+            _postgresDbContext = postgresDbContext;
+        }
+
+        public async Task<CreateChatEntityResponse> Handle(CreateGroupCommand request,
+            CancellationToken cancellationToken)
+        {
+            var requestMetadata = _metadataService.GetRequestMetadata();
+            var refreshTokenId = _cookieService.Get(CookieConstants.MangoRefreshTokenId);
+
+            var validationResult = await _jwtRefreshService.VerifyUserRefreshTokenAsync(refreshTokenId,
+                requestMetadata,
+                cancellationToken);
+
+            if (validationResult.IsSuspicious)
+            {
+                return CreateChatEntityResponse.Suspicious;
+            }
+
+            if (!validationResult.Success)
+            {
+                return CreateChatEntityResponse.RefreshTokenNotValidated;
+            }
+
+            if (request.GroupTitle == null)
+            {
+                return CreateChatEntityResponse.InvalidOrEmptyChatTitle;
+            }
+
+            if (request.GroupType == ChatType.DirectChat)
+            {
+                return CreateChatEntityResponse.InvalidGroupType;
+            }
+
+            var currentUser = await _postgresDbContext
+                .Users
+                .FirstAsync(x => x.Id == validationResult.RefreshToken.UserId, cancellationToken);
+
+            var directChatEntity = new ChatEntity
+            {
+                ChatType = request.GroupType,
+                Title = request.GroupTitle
+            };
+
+            await _postgresDbContext.Chats.AddAsync(directChatEntity, cancellationToken);
+            await _postgresDbContext.SaveChangesAsync(cancellationToken);
+
+            _postgresDbContext.UserChats.Add(new UserChatEntity
+            {
+                ChatId = directChatEntity.Id, RoleId = UserRole.Owner, UserId = currentUser.Id
+            });
+
+            await _postgresDbContext.SaveChangesAsync(cancellationToken);
+
+            return CreateChatEntityResponse.DirectChatCreateSuccess;
         }
     }
 }
