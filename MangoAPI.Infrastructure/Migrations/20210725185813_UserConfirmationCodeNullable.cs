001bcb211194d7a1b70c292702d18d9914083ddb
Petro Kolosov <pkolosov@openskydata.com>
2021-07-25 20:59:29 +0200

    user confirmation code is nullable

---
+++ MangoAPI.Infrastructure/Migrations/20210725185813_UserConfirmationCodeNullable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MangoAPI.Infrastructure.Migrations
+{
+    public partial class UserConfirmationCodeNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ConfirmationCode",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ConfirmationCode",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
