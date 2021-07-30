31a537b63b2e1b7b3d586df760cd736f5b0fa794
Petro Kolosov <pkolosov@openskydata.com>
2021-07-30 22:08:58 +0200

    minor edits

--- MangoAPI.Domain/Entities/UserEntity.cs
+++ MangoAPI.Domain/Entities/UserEntity.cs
@@ -3,15 +3,15 @@ using Microsoft.AspNetCore.Identity;
 
 namespace MangoAPI.Domain.Entities
 {
-    public class UserEntity : IdentityUser
+    public sealed class UserEntity : IdentityUser
     {
         public string DisplayName { get; set; }
         public string Image { get; set; }
         public string Bio { get; set; }
         public int? ConfirmationCode { get; set; }
-        public virtual ICollection<RefreshTokenEntity> RefreshTokens { get; set; }
-        public virtual ICollection<MessageEntity> Messages { get; set; }
-        public virtual ICollection<UserChatEntity> UserChats { get; set; }
+        public ICollection<RefreshTokenEntity> RefreshTokens { get; set; }
+        public ICollection<MessageEntity> Messages { get; set; }
+        public ICollection<UserChatEntity> UserChats { get; set; }
 
         public bool Verified => EmailConfirmed || PhoneNumberConfirmed;
     }
