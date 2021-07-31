2548cfef9dbe2d2b78f517e64cb7daab9a1a563d
Petro Kolosov <pkolosov@openskydata.com>
2021-07-31 12:54:34 +0200

    information and contacts tables added

---
+++ MangoAPI.Domain/Entities/UserInformationEntity.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using System;
+
+namespace MangoAPI.Domain.Entities
+{
+    public class UserInformationEntity
+    {
+        public string Id { get; set; }
+        public string? UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDay { get; set; }
+
+        public string? Website { get; set; }
+        public string? Address { get; set; }
+
+        public string? Facebook { get; set; }
+        public string? Twitter { get; set; }
+        public string? Instagram { get; set; }
+        public string? LinkedIn { get; set; }
+
+        public string? ProfilePicture { get; set; }
+
+        public virtual UserEntity User { get; set; }
+    }
+}
---
+++ MangoAPI.Infrastructure/Database/Configurations/UserInformationEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using MangoAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MangoAPI.Infrastructure.Database.Configurations
+{
+    public class UserInformationEntityConfiguration : IEntityTypeConfiguration<UserInformationEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserInformationEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
