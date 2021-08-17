3eeeed4dd24de17390db3e646b88f33b8784b016
Petro Kolosov <pkolosov@openskydata.com>
2021-08-17 10:51:11 +0200

    configurations added:

---
+++ MangoAPI.DataAccess/Database/Configurations/IdentityUserRoleConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using MangoAPI.Domain.Constants;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MangoAPI.DataAccess.Database.Configurations
+{
+    public class IdentityUserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.HasData(new IdentityUserRole<string>
+                {
+                    RoleId = SeedDataConstants.AdminRoleId,
+                    UserId = SeedDataConstants.RazumovskyId
+                }
+            );
+        }
+    }
+}
--- MangoAPI.DataAccess/Database/MangoPostgresDbContext.cs
+++ MangoAPI.DataAccess/Database/MangoPostgresDbContext.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using MangoAPI.Domain.Constants;
 using MangoAPI.Domain.Entities;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -28,32 +26,6 @@ namespace MangoAPI.DataAccess.Database
         {
             base.OnModelCreating(builder);
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-
-            builder.Entity<IdentityRole>().HasData(new IdentityRole
-                {
-                    Id = SeedDataConstants.AdminRoleId,
-                    Name = SeedDataConstants.AdminRole,
-                    NormalizedName = SeedDataConstants.AdminRole.ToUpper()
-                },
-                new IdentityRole
-                {
-                    Id = SeedDataConstants.UserRoleId,
-                    Name = SeedDataConstants.UserRole,
-                    NormalizedName = SeedDataConstants.UserRole.ToUpper()
-                },
-                new IdentityRole
-                {
-                    Id = SeedDataConstants.UnverifiedRoleId,
-                    Name = SeedDataConstants.UnverifiedRole,
-                    NormalizedName = SeedDataConstants.UnverifiedRole.ToUpper()
-                });
-
-            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-                {
-                    RoleId = SeedDataConstants.AdminRoleId,
-                    UserId = SeedDataConstants.RazumovskyId
-                }
-            );
         }
     }
 }
