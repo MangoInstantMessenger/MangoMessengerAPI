657fa46603d1fe55807496438f3b048394280311
Petro Kolosov <pkolosov@openskydata.com>
2021-08-23 09:46:06 +0200

    feature: in progress

---
+++ MangoAPI.BusinessLogic/ApiCommands/UserChats/ArchiveChatCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MangoAPI.BusinessLogic.ApiCommands.UserChats
+{
+    public class ArchiveChatCommandHandler : IRequestHandler<ArchiveChatCommand, ArchiveChatResponse>
+    {
+        public async Task<ArchiveChatResponse> Handle(ArchiveChatCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MangoAPI.BusinessLogic/ApiCommands/UserChats/ArchiveChatCommandValiadtor.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+
+namespace MangoAPI.BusinessLogic.ApiCommands.UserChats
+{
+    public class ArchiveChatCommandValiadtor : AbstractValidator<ArchiveChatCommand>
+    {
+        public ArchiveChatCommandValiadtor()
+        {
+            RuleFor(x => x.ChatId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .Length(1, 300);
+
+            RuleFor(x => x.ChatId).Must(x => Guid.TryParse(x, out _))
+                .WithMessage("ArchiveChatCommand: Chat Id cannot be parsed.");
+
+            RuleFor(x => x.UserId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .Length(1, 300);
+
+            RuleFor(x => x.UserId).Must(x => Guid.TryParse(x, out _))
+                .WithMessage("ArchiveChatCommand: User Id cannot be parsed.");
+
+            RuleFor(x => x.Archived).NotEmpty();
+        }
+    }
+}
