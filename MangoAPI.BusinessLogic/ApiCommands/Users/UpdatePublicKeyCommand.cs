b66c2cbb10d34308d99f92ec493936c0c402e0df
Petro Kolosov <pkolosov@openskydata.com>
2021-09-05 12:32:24 +0200

    empty files initialized

---
+++ MangoAPI.BusinessLogic/ApiCommands/Users/UpdatePublicKeyCommand.cs
@@ -0,0 +1,17 @@
+﻿using MangoAPI.BusinessLogic.Responses;
+using MediatR;
+
+namespace MangoAPI.BusinessLogic.ApiCommands.Users
+{
+    public record UpdatePublicKeyCommand : IRequest<ResponseBase>
+    {
+        public string UserId { get; }
+        public int PublicKey { get; }
+
+        public UpdatePublicKeyCommand(string userId, int publicKey)
+        {
+            UserId = userId;
+            PublicKey = publicKey;
+        }
+    }
+}
---
+++ MangoAPI.BusinessLogic/ApiCommands/Users/UpdatePublicKeyCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MangoAPI.BusinessLogic.Responses;
+using MediatR;
+
+namespace MangoAPI.BusinessLogic.ApiCommands.Users
+{
+    public class UpdatePublicKeyCommandHandler : IRequestHandler<UpdatePublicKeyCommand, ResponseBase>
+    {
+        public async Task<ResponseBase> Handle(UpdatePublicKeyCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
