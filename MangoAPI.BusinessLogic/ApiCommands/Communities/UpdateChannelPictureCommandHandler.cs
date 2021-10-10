665a3ba068ab5f8c3b3f802d11e71c0a86a07d20
Petro Kolosov <pkolosov@openskydata.com>
2021-10-10 03:57:53 +0200

    update channel picture updated

--- MangoAPI.BusinessLogic/ApiCommands/Communities/UpdateChannelPictureCommandHandler.cs
+++ MangoAPI.BusinessLogic/ApiCommands/Communities/UpdateChannelPictureCommandHandler.cs
@@ -1,12 +1,12 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using MangoAPI.BusinessLogic.BusinessExceptions;
+﻿using MangoAPI.BusinessLogic.BusinessExceptions;
 using MangoAPI.BusinessLogic.Responses;
 using MangoAPI.DataAccess.Database;
 using MangoAPI.DataAccess.Database.Extensions;
 using MangoAPI.Domain.Constants;
 using MangoAPI.Domain.Enums;
 using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MangoAPI.BusinessLogic.ApiCommands.Communities
 {
@@ -18,16 +18,9 @@ namespace MangoAPI.BusinessLogic.ApiCommands.Communities
         {
             _postgresDbContext = postgresDbContext;
         }
-        
+
         public async Task<ResponseBase> Handle(UpdateChanelPictureCommand request, CancellationToken cancellationToken)
         {
-            var user = await _postgresDbContext.Users.FindUserByIdAsync(request.UserId, cancellationToken);
-            
-            if (user is null)
-            {
-               throw new BusinessException(ResponseMessageCodes.UserNotFound);
-            }
-
             var chat = await _postgresDbContext.Chats.FindChatByIdAsync(request.ChatId, cancellationToken);
 
             if (chat is null)
@@ -35,7 +28,7 @@ namespace MangoAPI.BusinessLogic.ApiCommands.Communities
                 throw new BusinessException(ResponseMessageCodes.ChatNotFound);
             }
 
-            if (chat.CommunityType == (int) CommunityType.DirectChat)
+            if (chat.CommunityType == (int)CommunityType.DirectChat)
             {
                 throw new BusinessException(ResponseMessageCodes.PermissionDenied);
             }
@@ -44,7 +37,7 @@ namespace MangoAPI.BusinessLogic.ApiCommands.Communities
 
             _postgresDbContext.Chats.Update(chat);
             await _postgresDbContext.SaveChangesAsync(cancellationToken);
-            
+
             return ResponseBase.SuccessResponse;
         }
     }
