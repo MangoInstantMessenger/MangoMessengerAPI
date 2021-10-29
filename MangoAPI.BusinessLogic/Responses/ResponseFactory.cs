8e43c8151e459102b7fd2b5ede59800a2c18a134
Petro Kolosov <pkolosov@openskydata.com>
2021-10-29 21:36:58 +0200

    feat: response factory initialized

---
+++ MangoAPI.BusinessLogic/Responses/ResponseFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+
+namespace MangoAPI.BusinessLogic.Responses
+{
+    public class ResponseFactory<TResponse> where TResponse : ResponseBase
+    {
+        public Result<TResponse> ConflictResponse(string message, string description)
+        {
+            return new Result<TResponse>
+            {
+                Error = new ErrorResponse
+                {
+                    ErrorMessage = message,
+                    ErrorDetails = description,
+                    Success = false,
+                    StatusCode = HttpStatusCode.Conflict
+                },
+                Response = null,
+                StatusCode = HttpStatusCode.Conflict
+            };
+        }
+
+        public Result<TResponse> SuccessResponse(TResponse response)
+        {
+            return new Result<TResponse>
+            {
+                Error = null,
+                Response = response,
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
