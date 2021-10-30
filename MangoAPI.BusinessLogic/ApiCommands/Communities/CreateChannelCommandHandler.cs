027419389ed8f2b7807cf89167dadde689140148
Хачатур <xachulxx@gmail.com>
2021-10-30 12:53:55 +0300

    refactor

--- MangoAPI.BusinessLogic/ApiCommands/Communities/CreateChannelCommandHandler.cs
+++ MangoAPI.BusinessLogic/ApiCommands/Communities/CreateChannelCommandHandler.cs
@@ -21,11 +21,14 @@ namespace MangoAPI.BusinessLogic.ApiCommands.Communities
     {
         private readonly MangoPostgresDbContext _postgresDbContext;
         private readonly IHubContext<ChatHub, IHubClient> _hubContext;
+        private readonly ResponseFactory<CreateCommunityResponse> _responseFactory;
 
-        public CreateChannelCommandHandler(MangoPostgresDbContext postgresDbContext, IHubContext<ChatHub, IHubClient> hubContext)
+        public CreateChannelCommandHandler(MangoPostgresDbContext postgresDbContext, 
+            IHubContext<ChatHub, IHubClient> hubContext, ResponseFactory<CreateCommunityResponse> responseFactory)
         {
             _postgresDbContext = postgresDbContext;
             _hubContext = hubContext;
+            _responseFactory = responseFactory;
         }
 
         public async Task<Result<CreateCommunityResponse>> Handle(CreateChannelCommand request,
@@ -33,19 +36,10 @@ namespace MangoAPI.BusinessLogic.ApiCommands.Communities
         {
             if (request.CommunityType is CommunityType.DirectChat or CommunityType.SecretChat)
             {
-                return new Result<CreateCommunityResponse>
-                {
-                    Error = new ErrorResponse
-                    {
-                        ErrorMessage = ResponseMessageCodes.InvalidGroupType,
-                        ErrorDetails = ResponseMessageCodes.ErrorDictionary[ResponseMessageCodes.InvalidGroupType],
-                        StatusCode = HttpStatusCode.Conflict,
-                        Success = false,
-                    },
+                const string message = ResponseMessageCodes.InvalidGroupType;
+                var description = ResponseMessageCodes.ErrorDictionary[message];
 
-                    Response = null,
-                    StatusCode = HttpStatusCode.Conflict
-                };
+                return _responseFactory.ConflictResponse(message, description);
             }
 
             var ownerChatsCount =
@@ -55,18 +49,10 @@ namespace MangoAPI.BusinessLogic.ApiCommands.Communities
 
             if (ownerChatsCount >= 100)
             {
-                return new Result<CreateCommunityResponse>
-                {
-                    Error = new ErrorResponse
-                    {
-                        ErrorMessage = ResponseMessageCodes.MaximumOwnerChatsExceeded100,
-                        ErrorDetails = ResponseMessageCodes.ErrorDictionary[ResponseMessageCodes.MaximumOwnerChatsExceeded100],
-                        Success = false,
-                        StatusCode = HttpStatusCode.Conflict
-                    },
-                    Response = null,
-                    StatusCode = HttpStatusCode.Conflict
-                };
+                const string message = ResponseMessageCodes.MaximumOwnerChatsExceeded100;
+                var description = ResponseMessageCodes.ErrorDictionary[message];
+
+                return _responseFactory.ConflictResponse(message, description);
             }
 
             var channel = new ChatEntity
