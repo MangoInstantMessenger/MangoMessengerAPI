0c5cfd377d9e3b2a8b2b48b9f1006fa1d82b6548
Petro Kolosov <kolosovp94@gmail.com>
2021-11-16 22:41:01 +0100

    get public keys implemented

---
+++ MangoAPI.BusinessLogic/ApiQueries/PublicKeys/GetPublicKeysQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MangoAPI.BusinessLogic.Responses;
+using MediatR;
+
+namespace MangoAPI.BusinessLogic.ApiQueries.PublicKeys
+{
+    public record GetPublicKeysQuery : IRequest<Result<GetPublicKeysResponse>>
+    {
+        public Guid UserId { get; init; }
+    }
+}
---
+++ MangoAPI.BusinessLogic/ApiQueries/PublicKeys/GetPublicKeysResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MangoAPI.BusinessLogic.Models;
+using MangoAPI.BusinessLogic.Responses;
+
+namespace MangoAPI.BusinessLogic.ApiQueries.PublicKeys
+{
+    public record GetPublicKeysResponse : ResponseBase
+    {
+        public List<PublicKey> PublicKeys { get; init; }
+    }
+}
