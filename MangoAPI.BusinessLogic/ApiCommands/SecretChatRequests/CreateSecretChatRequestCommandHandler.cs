99b44b4323554adf99ed74cc13014e90d9ecc503
Petro Kolosov <kolosovp94@gmail.com>
2021-11-16 16:01:03 +0100

    request secret chat implemented

--- MangoAPI.BusinessLogic/ApiCommands/SecretChatRequests/CreateSecretChatRequestCommandHandler.cs
+++ MangoAPI.BusinessLogic/ApiCommands/SecretChatRequests/CreateSecretChatRequestCommandHandler.cs
@@ -1,11 +1,11 @@
-﻿using System.Security.Cryptography;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using MangoAPI.BusinessLogic.Extensions;
 using MangoAPI.BusinessLogic.Responses;
 using MangoAPI.DataAccess.Database;
+using MangoAPI.Domain.Constants;
 using MangoAPI.Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace MangoAPI.BusinessLogic.ApiCommands.SecretChatRequests
 {
@@ -25,10 +25,32 @@ namespace MangoAPI.BusinessLogic.ApiCommands.SecretChatRequests
         public async Task<Result<ResponseBase>> Handle(CreateSecretChatRequestCommand request,
             CancellationToken cancellationToken)
         {
+            var requestedUserExists = await _postgresDbContext.Users.AnyAsync(
+                x => x.Id == request.RequestedUserId, cancellationToken);
+
+            if (!requestedUserExists)
+            {
+                const string message = ResponseMessageCodes.UserNotFound;
+                var details = ResponseMessageCodes.ErrorDictionary[message];
+
+                return _responseFactory.ConflictResponse(message, details);
+            }
+            
+            var alreadyRequested = await _postgresDbContext.SecretChatRequests
+                .AnyAsync(x => x.SenderId == request.UserId, cancellationToken);
+
+            if (alreadyRequested)
+            {
+                const string message = ResponseMessageCodes.SecretChatRequestAlreadyExists;
+                var details = ResponseMessageCodes.ErrorDictionary[message];
+                
+                return _responseFactory.ConflictResponse(message, details);
+            }
+
             var secretChatRequest = new SecretChatRequestEntity
             {
                 UserId = request.RequestedUserId,
-                SenderId = request.RequestedUserId,
+                SenderId = request.UserId,
                 SenderPublicKey = request.PublicKey
             };
 
--- MangoAPI.Presentation/Interfaces/ISecretChatRequestsController.cs
+++ MangoAPI.Presentation/Interfaces/ISecretChatRequestsController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using MangoAPI.BusinessLogic.ApiCommands.SecretChatRequests;
 using Microsoft.AspNetCore.Mvc;
