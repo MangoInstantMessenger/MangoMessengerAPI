7b479c3bff7e54884cd5d155084d297b4cfa4035
Petro Kolosov <kolosovp94@gmail.com>
2021-12-26 21:06:43 +0100

    updated

--- MangoAPI.BusinessLogic/ApiCommands/Documents/UploadDocumentCommandValidator.cs
+++ MangoAPI.BusinessLogic/ApiCommands/Documents/UploadDocumentCommandValidator.cs
@@ -1,32 +1,48 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using FluentValidation;
 
 namespace MangoAPI.BusinessLogic.ApiCommands.Documents
 {
     public class UploadDocumentCommandValidator : AbstractValidator<UploadDocumentCommand>
     {
+        private readonly List<string> _allowedExtensions = new()
+        {
+            "jpg", "JPG", "txt", "TXT", "pdf", "PDF", "gif", "GIF", "png", "PNG"
+        };
+
+
         public UploadDocumentCommandValidator()
         {
-            var allowedExtensions = new List<string>
-            {
-                "jpg", "JPG", "txt", "TXT", "pdf", "PDF", "gif", "GIF", "png", "PNG"
-            };
-
             RuleFor(x => x.FormFile).NotEmpty();
-            RuleFor(x => x.FormFile.Length).LessThanOrEqualTo(10 * 1024 * 1024);
+            RuleFor(x => x.FormFile.Length)
+                .GreaterThan(0)
+                .LessThanOrEqualTo(10 * 1024 * 1024);
 
             RuleFor(x => x.FormFile.FileName)
                 .Cascade(CascadeMode.Stop)
                 .NotEmpty()
-                .Must(t =>
-                {
-                    var validExtension = t.Split('.').Last();
-                    return allowedExtensions.Contains(validExtension);
-                })
+                .Must(HaveAllowedExtension)
+                .WithMessage(
+                    $"File extension is not allowed. Allowed extensions: {string.Join(", ", _allowedExtensions)}.")
+                .Must(HaveValidName)
+                .WithMessage("Invalid file name.")
                 .Length(1, 20);
 
             RuleFor(x => x.UserId).NotEmpty();
+        }
+
+        private static bool HaveValidName(string str)
+        {
+            var regex = new Regex(@"^[\w\-. ]+$");
+            return regex.IsMatch(str);
+        }
+
+        private bool HaveAllowedExtension(string str)
+        {
+            var extension = str.Split('.').Last();
+            return _allowedExtensions.Contains(extension);
         }
     }
 }
