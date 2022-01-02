5cb3c3d523e47a7f0e2c34ba624b24e671f3fe23
Petro Kolosov <kolosovp94@gmail.com>
2022-01-02 20:06:11 +0100

    send message handler tests are finished

--- MangoAPI.Tests/ApiCommandsTests/SendMessageCommandHandlerTests/SendMessageShouldThrowUserNotFound.cs
+++ MangoAPI.Tests/ApiCommandsTests/SendMessageCommandHandlerTests/SendMessageShouldThrowUserNotFound.cs
@@ -1,5 +1,10 @@
-﻿using MangoAPI.BusinessLogic.ApiCommands.Messages;
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MangoAPI.BusinessLogic.ApiCommands.Messages;
 using MangoAPI.BusinessLogic.Responses;
+using MangoAPI.Domain.Constants;
 using MediatR;
 using NUnit.Framework;
 
@@ -8,14 +13,38 @@ namespace MangoAPI.Tests.ApiCommandsTests.SendMessageCommandHandlerTests
     [TestFixture]
     public class SendMessageShouldThrowUserNotFound : ITestable<SendMessageCommand, SendMessageResponse>
     {
+        private readonly MangoDbFixture _mangoDbFixture = new();
+
+        [Test]
+        public async Task SendMessage_ShouldThrow_UserNotFound()
+        {
+            var handler = CreateHandler();
+            const string expectedMessage = ResponseMessageCodes.UserNotFound;
+            var expectedDetails = ResponseMessageCodes.ErrorDictionary[expectedMessage];
+            var sendMessageCommand = new SendMessageCommand
+            {
+                ChatId = SeedDataConstants.ExtremeCodeMainId,
+                UserId = SeedDataConstants.RazumovskyId,
+                MessageText = "This is test message"
+            };
+
+            var result = await handler.Handle(sendMessageCommand, CancellationToken.None);
+
+            result.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            result.Error.ErrorMessage.Should().Be(expectedMessage);
+            result.Error.ErrorDetails.Should().Be(expectedDetails);
+        }
+
         public void Seed()
         {
-            throw new System.NotImplementedException();
         }
 
         public IRequestHandler<SendMessageCommand, Result<SendMessageResponse>> CreateHandler()
         {
-            throw new System.NotImplementedException();
+            var hubContext = MockedObjects.GetHubContext();
+            var responseFactory = new ResponseFactory<SendMessageResponse>();
+            var handler = new SendMessageCommandHandler(_mangoDbFixture.Context, hubContext, responseFactory);
+            return handler;
         }
     }
 }
