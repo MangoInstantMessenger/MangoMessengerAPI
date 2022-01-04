191c7fd91872ab813042cc8d4be40623dd1595e9
Хачатур <xachulxx@gmail.com>
2022-01-04 20:51:34 +0300

    feature: writed unit tests for api/password-restore-request PUT

--- MangoAPI.Tests/MockedObjects.cs
+++ MangoAPI.Tests/MockedObjects.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using MangoAPI.Application.Interfaces;
 using MangoAPI.BusinessLogic.HubConfig;
 using MangoAPI.BusinessLogic.Models;
+using MangoAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Moq;
@@ -55,6 +58,27 @@ namespace MangoAPI.Tests
                 .Returns(Task.FromResult(blobName));
 
             return blobServiceMock.Object;
+        }
+
+        public static IEmailSenderService GetEmailSenderServiceMock()
+        {
+            var emailSenderMock = new Mock<IEmailSenderService>();
+
+            emailSenderMock.Setup(emailSender =>
+                emailSender.SendVerificationEmailAsync(
+                    It.IsAny<UserEntity>(),
+                    It.IsAny<CancellationToken>()
+                )
+            ).Returns(Task.CompletedTask);
+
+            emailSenderMock.Setup(emailSender =>
+                emailSender.SendPasswordRestoreRequestAsync(It.IsAny<UserEntity>(),
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()
+                )
+            ).Returns(Task.CompletedTask);
+
+            return emailSenderMock.Object;
         }
     }
 }
