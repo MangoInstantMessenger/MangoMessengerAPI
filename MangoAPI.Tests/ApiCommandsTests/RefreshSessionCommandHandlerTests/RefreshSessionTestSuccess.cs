fa6ca673944537ea82cb1f52bca961b63765c211
Хачатур <xachulxx@gmail.com>
2022-01-07 20:24:38 +0300

    commit

--- MangoAPI.Tests/ApiCommandsTests/RefreshSessionCommandHandlerTests/RefreshSessionTestSuccess.cs
+++ MangoAPI.Tests/ApiCommandsTests/RefreshSessionCommandHandlerTests/RefreshSessionTestSuccess.cs
@@ -29,9 +29,6 @@ namespace MangoAPI.Tests.ApiCommandsTests.RefreshSessionCommandHandlerTests
             };
 
             var result = await handler.Handle(command, CancellationToken.None);
-
-            Console.WriteLine(result.Error.ErrorMessage);
-            Console.WriteLine(result.StatusCode);
             
             result.StatusCode.Should().Be(HttpStatusCode.OK);
             result.Response.Success.Should().BeTrue();
