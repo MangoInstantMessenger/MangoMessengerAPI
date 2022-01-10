6c1d7ee8a849b238f9da94939ef66e534145a46c
Petro Kolosov <kolosovp94@gmail.com>
2022-01-10 14:16:06 +0100

    user managed moved under interface

---
+++ MangoAPI.Application/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using MangoAPI.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace MangoAPI.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IdentityResult> CreateAsync(UserEntity user, string password);
+    }
+}
