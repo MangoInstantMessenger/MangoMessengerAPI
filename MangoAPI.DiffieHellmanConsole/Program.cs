3e98b518ec984e19644951bcb8ea701859924b5d
kolosovpetro <kolosovp94@gmail.com>
2022-03-17 22:47:08 +0100

    exception is thrown when handler null

--- MangoAPI.DiffieHellmanConsole/Program.cs
+++ MangoAPI.DiffieHellmanConsole/Program.cs
@@ -27,44 +27,64 @@ public static class Program
         {
             case "login":
             {
-                var handler = serviceProvider.GetService<LoginHandler>();
+                var handler = serviceProvider.GetService<LoginHandler>() ??
+                              throw new ArgumentException(
+                                  $"Handler is null. Register it in dependency injection. {nameof(LoginHandler)}");
+
                 await handler.LoginAsync(args);
                 break;
             }
             case "refresh-token":
             {
-                var refreshHandler = serviceProvider.GetService<RefreshTokenHandler>();
+                var refreshHandler = serviceProvider.GetService<RefreshTokenHandler>() ??
+                                     throw new ArgumentException(
+                                         $"Handler is null. Register it in dependency injection. {nameof(RefreshTokenHandler)}");
+
                 await refreshHandler.RefreshTokensAsync();
                 break;
             }
             case "key-exchange":
             {
-                var handler = serviceProvider.GetService<CngRequestKeyExchangeHandler>();
+                var handler = serviceProvider.GetService<CngRequestKeyExchangeHandler>() ??
+                              throw new ArgumentException(
+                                  $"Handler is null. Register it in dependency injection. {nameof(CngRequestKeyExchangeHandler)}");
+
                 await handler.RequestKeyExchange(args);
                 break;
             }
             case "key-exchange-requests":
             {
-                var handler = serviceProvider.GetService<PrintKeyExchangeListHandler>();
+                var handler = serviceProvider.GetService<PrintKeyExchangeListHandler>() ??
+                              throw new ArgumentException(
+                                  $"Handler is null. Register it in dependency injection. {nameof(PrintKeyExchangeListHandler)}");
 
                 await handler.PrintKeyExchangesListAsync();
                 break;
             }
             case "confirm-key-exchange":
             {
-                var handler = serviceProvider.GetService<CngConfirmKeyExchangeRequestHandler>();
+                var handler = serviceProvider.GetService<CngConfirmKeyExchangeRequestHandler>() ??
+                              throw new ArgumentException(
+                                  $"Handler is null. Register it in dependency injection. {nameof(CngConfirmKeyExchangeRequestHandler)}");
+
                 await handler.ConfirmKeyExchangeRequest(args);
                 break;
             }
             case "print-public-keys":
             {
-                var handler = serviceProvider.GetService<PrintPublicKeysHandler>();
+                var handler = serviceProvider.GetService<PrintPublicKeysHandler>() ??
+                              throw new ArgumentException(
+                                  $"Handler is null. Register it in dependency injection. {nameof(PrintPublicKeysHandler)}");
+
                 await handler.PrintPublicKeysAsync();
                 break;
             }
             case "create-common-secret":
             {
-                var handler = serviceProvider.GetService<CngCreateCommonSecretHandler>();
+                var handler = serviceProvider.GetService<CngCreateCommonSecretHandler>() ??
+                              throw new ArgumentException(
+                                  $"Handler is null. Register it in dependency injection. {nameof(CngCreateCommonSecretHandler)}");
+                
                 await handler.CreateCommonSecret(args);
                 break;
             }
