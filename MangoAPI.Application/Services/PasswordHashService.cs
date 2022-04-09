9e0ab7ac2cec2773cc85ffca128640faa11c7dac
Vladislav <vlad356102@yandex.ru>
2022-04-09 13:57:58 +0300

    deleting of my comment

--- MangoAPI.Application/Services/PasswordHashService.cs
+++ MangoAPI.Application/Services/PasswordHashService.cs
@@ -7,7 +7,6 @@ public class PasswordHashService : PasswordHasher<UserEntity>
 {
     public override string HashPassword(UserEntity user, string password)
     {
-        //This looks strange a bit. I wanna delete it or make it like an extension method.
         user.PasswordHash = base.HashPassword(user, password);
         
         return user.PasswordHash;
