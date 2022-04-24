d30de319f33c2d0785589a9b8cd67ab059f3d295
Khachatur Khachatryan <xachulxx@gmail.com>
2022-04-24 19:49:57 +0300

    bugfix: letter case in ErrorResponse

--- MangoAPI.BusinessLogic/Responses/ErrorResponse.cs
+++ MangoAPI.BusinessLogic/Responses/ErrorResponse.cs
@@ -1,22 +1,26 @@
 ﻿using System.ComponentModel;
 using System.Net;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace MangoAPI.BusinessLogic.Responses;
 
 public record ErrorResponse
 {
     [DefaultValue("ERROR_MESSAGE")]
+    [JsonProperty("errorMessage")]
     public string ErrorMessage { get; init; }
         
     [DefaultValue("Error description")]
+    [JsonProperty("errorDetails")]
     public string ErrorDetails { get; init; }
         
     [DefaultValue(409)]
+    [JsonProperty("statusCode")]
     public HttpStatusCode StatusCode { get; init; }
         
     [DefaultValue(false)]
+    [JsonProperty("success")]
     public bool Success { get; init; }
 
-    public override string ToString() => JsonSerializer.Serialize(this);
+    public override string ToString() => JsonConvert.SerializeObject(this);
 }
