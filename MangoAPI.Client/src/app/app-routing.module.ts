18ca889e0503a4494ebe0b5d0e16ba149777e474
kolosovpetro <kolosovp94@gmail.com>
2022-05-05 10:54:29 +0200

    prettier configured, lf end of line configured

--- MangoAPI.Client/src/app/app-routing.module.ts
+++ MangoAPI.Client/src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {FirstComponent} from './first/first.component';
-import {SecondComponent} from './second/second.component';
-import {AppComponent} from "./app.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: 'first-component', component: FirstComponent},
-  {path: 'second-component', component: SecondComponent},
-  {path: 'app', component: AppComponent},
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+  { path: 'app', component: AppComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
