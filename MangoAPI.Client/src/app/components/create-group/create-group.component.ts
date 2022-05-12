c5eb3e65739ff31d7bc658de6912e91419b092fa
Khachatur Khachatryan <xachulxx@gmail.com>
2022-05-12 18:43:47 +0300

    feature: added creeate group component

---
+++ MangoAPI.Client/src/app/components/create-group/create-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-group',
+  templateUrl: './create-group.component.html',
+  styleUrls: ['./create-group.component.scss']
+})
+export class CreateGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
