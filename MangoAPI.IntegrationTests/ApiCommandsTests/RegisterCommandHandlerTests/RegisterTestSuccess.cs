89a7838a2c990e496a8e06af59b620763a279920
kolosovpetro <kolosovp94@gmail.com>
2022-06-05 14:28:44 +0200

    register test success is done

--- MangoAPI.IntegrationTests/ApiCommandsTests/RegisterCommandHandlerTests/RegisterTestSuccess.cs
+++ MangoAPI.IntegrationTests/ApiCommandsTests/RegisterCommandHandlerTests/RegisterTestSuccess.cs
@@ -1,55 +1,22 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MangoAPI.BusinessLogic.ApiCommands.Users;
-using MangoAPI.BusinessLogic.Responses;
-using MediatR;
+using MangoAPI.IntegrationTests.Helpers;
 using Xunit;
 
 namespace MangoAPI.IntegrationTests.ApiCommandsTests.RegisterCommandHandlerTests;
 
-public class RegisterTestSuccess : ITestable<RegisterCommand, RegisterResponse>
+public class RegisterTestSuccess : IntegrationTestBase
 {
-    private readonly MangoDbFixture _mangoDbFixture = new();
     private readonly Assert<RegisterResponse> _assert = new();
 
     [Fact]
     public async Task RegisterTest_Success()
     {
-        Seed();
-        var handler = CreateHandler();
+        var command = CommandHelper.RegisterPetroCommand();
 
-        var result = await handler.Handle(_command, CancellationToken.None);
+        var regResult = await MangoModule.RequestAsync(command, CancellationToken.None);
 
-        _assert.Pass(result);
+        _assert.Pass(regResult);
     }
-
-    public bool Seed()
-    {
-        return true;
-    }
-
-    public IRequestHandler<RegisterCommand, Result<RegisterResponse>> CreateHandler()
-    {
-        var emailSenderServiceMock = MockedObjects.GetEmailSenderServiceMock();
-        var userServiceMock = MockedObjects.GetUserServiceMock(_command.Password);
-        var responseFactory = new ResponseFactory<RegisterResponse>();
-        var mailSettings = MockedObjects.GetMailgunSettings();
-
-        var handler = new RegisterCommandHandler(
-            userServiceMock,
-            _mangoDbFixture.Context,
-            emailSenderServiceMock,
-            responseFactory,
-            mailSettings);
-
-        return handler;
-    }
-
-    private readonly RegisterCommand _command = new()
-    {
-        Email = "kolosovp95@gmail.com",
-        DisplayName = "Petro Kolosov",
-        Password = "Bm3-`dPRv-/w#3)cw^97",
-        TermsAccepted = true
-    };
 }
