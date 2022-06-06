cd358205f6fc2808452da15cac97a62b26cbf499
Khachatur Khachatryan <xachulxx@gmail.com>
2022-06-06 22:26:56 +0300

    feature: implemented integration tests for api/messages POST

--- MangoAPI.IntegrationTests/ApiCommandsTests/SendMessageCommandHandlerTests/SendMessageShouldThrowUserNotFound.cs
+++ MangoAPI.IntegrationTests/ApiCommandsTests/SendMessageCommandHandlerTests/SendMessageShouldThrowUserNotFound.cs
@@ -1,47 +1,33 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using MangoAPI.BusinessLogic.ApiCommands.Messages;
-using MangoAPI.BusinessLogic.Responses;
 using MangoAPI.Domain.Constants;
-using MediatR;
+using MangoAPI.IntegrationTests.Helpers;
 using Xunit;
 
 namespace MangoAPI.IntegrationTests.ApiCommandsTests.SendMessageCommandHandlerTests;
 
-public class SendMessageShouldThrowUserNotFound : ITestable<SendMessageCommand, SendMessageResponse>
+public class SendMessageShouldThrowUserNotFound : IntegrationTestBase
 {
-    private readonly MangoDbFixture _mangoDbFixture = new();
     private readonly Assert<SendMessageResponse> _assert = new();
 
     [Fact]
     public async Task SendMessage_ShouldThrow_UserNotFound()
     {
-        var handler = CreateHandler();
         const string expectedMessage = ResponseMessageCodes.UserNotFound;
         var expectedDetails = ResponseMessageCodes.ErrorDictionary[expectedMessage];
-        var command = new SendMessageCommand
-        {
-            ChatId = SeedDataConstants.ExtremeCodeMainId,
-            UserId = SeedDataConstants.RazumovskyId,
-            MessageText = "This is test message"
-        };
-            
-        var result = await handler.Handle(command, CancellationToken.None);
+        var user = await MangoModule.RequestAsync(
+            request: CommandHelper.RegisterPetroCommand(),
+            cancellationToken: CancellationToken.None);
+        var chat = await MangoModule.RequestAsync(
+            request: CommandHelper.CreateExtremeCodeMainChatCommand(user.Response.UserId),
+            cancellationToken: CancellationToken.None);
+
+        var result = await MangoModule.RequestAsync(
+            request: CommandHelper.SendMessageToChannelCommand(Guid.NewGuid(), chat.Response.ChatId),
+            cancellationToken: CancellationToken.None);
 
         _assert.Fail(result, expectedMessage, expectedDetails);
-    }
-
-    public bool Seed()
-    {
-        return true;
-    }
-
-    public IRequestHandler<SendMessageCommand, Result<SendMessageResponse>> CreateHandler()
-    {
-        var hubContext = MockedObjects.GetHubContextMock();
-        var responseFactory = new ResponseFactory<SendMessageResponse>();
-        var blobSettings = MockedObjects.GetBlobServiceSettingsMock();
-        var handler = new SendMessageCommandHandler(_mangoDbFixture.Context, hubContext, responseFactory, blobSettings);
-        return handler;
     }
 }
--- MangoAPI.IntegrationTests/ApiCommandsTests/SendMessageCommandHandlerTests/SendMessageSuccessTest.cs
+++ MangoAPI.IntegrationTests/ApiCommandsTests/SendMessageCommandHandlerTests/SendMessageSuccessTest.cs
@@ -1,103 +1,41 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MangoAPI.BusinessLogic.ApiCommands.Messages;
-using MangoAPI.BusinessLogic.Responses;
-using MangoAPI.Domain.Constants;
-using MangoAPI.Domain.Entities;
-using MangoAPI.Domain.Enums;
-using MediatR;
+using MangoAPI.IntegrationTests.Helpers;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace MangoAPI.IntegrationTests.ApiCommandsTests.SendMessageCommandHandlerTests;
 
-public class SendMessageSuccessTest : ITestable<SendMessageCommand, SendMessageResponse>
+public class SendMessageSuccessTest : IntegrationTestBase
 {
-    private readonly MangoDbFixture _mangoDbFixture = new();
     private readonly Assert<SendMessageResponse> _assert = new();
 
     [Fact]
     public async Task SendMessage_Test_Success()
     {
-        await using var context = _mangoDbFixture.Context;
-        Seed();
-        var handler = CreateHandler();
+        var user = await MangoModule.RequestAsync(
+            request: CommandHelper.RegisterPetroCommand(),
+            cancellationToken: CancellationToken.None);
+        var chat = await MangoModule.RequestAsync(
+            request: CommandHelper.CreateExtremeCodeMainChatCommand(user.Response.UserId),
+            cancellationToken: CancellationToken.None);
 
-        var result = await handler.Handle(_command, CancellationToken.None);
+        var result = await MangoModule.RequestAsync(
+            request: CommandHelper.SendMessageToChannelCommand(user.Response.UserId, chat.Response.ChatId),
+            cancellationToken: CancellationToken.None);;
 
         _assert.Pass(result);
-        var chat = context.Chats.AsNoTracking().First(x => x.Id == _chat.Id);
-        chat.LastMessageText.Should().Be(_command.MessageText);
-        chat.LastMessageId.Should().Be(result.Response.MessageId);
-        chat.LastMessageAuthor.Should().Be(_user.DisplayName);
+        var messageEntity = await DbContextFixture.Messages
+            .Include(x => x.User)
+            .Include(x => x.Chat)
+            .FirstOrDefaultAsync(x => x.Id == result.Response.MessageId);
+        var chatEntity = messageEntity.Chat;
+        var userEntity = messageEntity.User;
+        chatEntity.LastMessageAuthor.Should().Be(userEntity.DisplayName);
+        chatEntity.LastMessageId.Should().Be(messageEntity.Id);
+        chatEntity.LastMessageText.Should().Be(messageEntity.Content);
+        chatEntity.LastMessageTime.Should().Be(messageEntity.CreatedAt);
     }
-
-    public bool Seed()
-    {
-        _mangoDbFixture.Context.Users.Add(_user);
-        _mangoDbFixture.Context.Chats.Add(_chat);
-        _mangoDbFixture.Context.UserChats.Add(_userChatEntity);
-
-        _mangoDbFixture.Context.SaveChanges();
-
-        _mangoDbFixture.Context.Entry(_user).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_chat).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_userChatEntity).State = EntityState.Detached;
-
-        return true;
-    }
-
-    public IRequestHandler<SendMessageCommand, Result<SendMessageResponse>> CreateHandler()
-    {
-        var hubContext = MockedObjects.GetHubContextMock();
-        var responseFactory = new ResponseFactory<SendMessageResponse>();
-        var blobSettings = MockedObjects.GetBlobServiceSettingsMock();
-        var handler = new SendMessageCommandHandler(_mangoDbFixture.Context, hubContext, responseFactory, blobSettings);
-        return handler;
-    }
-
-    private readonly ChatEntity _chat = new()
-    {
-        Id = SeedDataConstants.ExtremeCodeMainId,
-        Title = "Extreme Code Main",
-        CommunityType = (int) CommunityType.PublicChannel,
-        Description = "Extreme Code Main Public Group",
-        CreatedAt = new DateTime(2020, 2, 4),
-        MembersCount = 4,
-        Image = "extreme_code_main.jpg",
-        UpdatedAt = DateTime.UtcNow,
-        LastMessageAuthor = "Amelit",
-        LastMessageText = "TypeScript The Best",
-        LastMessageTime = DateTime.Parse("2:32 PM")
-    };
-
-    private readonly UserEntity _user = new()
-    {
-        DisplayName = "razumovsky r",
-        Bio = "11011 y.o Dotnet Developer from $\"{cityName}\"",
-        Id = SeedDataConstants.RazumovskyId,
-        UserName = "razumovsky_r",
-        Email = "kolosovp95@gmail.com",
-        NormalizedEmail = "KOLOSOVP94@GMAIL.COM",
-        EmailConfirmed = true,
-        PhoneNumberConfirmed = true,
-        Image = "razumovsky_picture.jpg"
-    };
-
-    private readonly UserChatEntity _userChatEntity = new()
-    {
-        UserId = SeedDataConstants.RazumovskyId,
-        ChatId = SeedDataConstants.ExtremeCodeMainId,
-        RoleId = (int) UserRole.Admin,
-    };
-
-    private readonly SendMessageCommand _command = new()
-    {
-        ChatId = SeedDataConstants.ExtremeCodeMainId,
-        UserId = SeedDataConstants.RazumovskyId,
-        MessageText = "This is test message"
-    };
 }
