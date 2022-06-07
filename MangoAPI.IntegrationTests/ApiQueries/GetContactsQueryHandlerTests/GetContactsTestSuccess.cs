caab00d3efeec0b563b56fcc9ad2e1d05cc60470
Khachatur Khachatryan <xachulxx@gmail.com>
2022-06-07 15:20:51 +0300

    feature: implemented integration tests for api/contacts GET

--- MangoAPI.IntegrationTests/ApiQueries/GetContactsQueryHandlerTests/GetContactsTestSuccess.cs
+++ MangoAPI.IntegrationTests/ApiQueries/GetContactsQueryHandlerTests/GetContactsTestSuccess.cs
@@ -2,17 +2,19 @@
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
+using MangoAPI.BusinessLogic;
 using MangoAPI.BusinessLogic.ApiQueries.Contacts;
 using MangoAPI.BusinessLogic.Responses;
 using MangoAPI.Domain.Constants;
 using MangoAPI.Domain.Entities;
+using MangoAPI.IntegrationTests.Helpers;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace MangoAPI.IntegrationTests.ApiQueries.GetContactsQueryHandlerTests;
 
-public class GetContactsTestSuccess : ITestable<GetContactsQuery, GetContactsResponse>
+public class GetContactsTestSuccess : IntegrationTestBase
 {
     private readonly MangoDbFixture _mangoDbFixture = new();
     private readonly Assert<GetContactsResponse> _assert = new();
@@ -20,18 +22,25 @@ public class GetContactsTestSuccess : ITestable<GetContactsQuery, GetContactsRes
     [Fact]
     public async Task GetContactsTest_Success()
     {
-        Seed();
-        var handler = CreateHandler();
+        var user = await MangoModule.RequestAsync(
+            request: CommandHelper.RegisterPetroCommand(),
+            cancellationToken: CancellationToken.None);
+        var contact = await MangoModule.RequestAsync(
+            request: CommandHelper.RegisterKhachaturCommand(),
+            cancellationToken: CancellationToken.None);
+        await MangoModule.RequestAsync(
+            request: CommandHelper.CreateContactCommand(user.Response.UserId, contact.Response.UserId),
+            cancellationToken: CancellationToken.None);
         var query = new GetContactsQuery
         {
-            UserId = SeedDataConstants.KhachaturId
+            UserId = user.Response.UserId
         };
 
-        var result = await handler.Handle(query, CancellationToken.None);
+        var result = await MangoModule.RequestAsync(query, CancellationToken.None);
             
         _assert.Pass(result);
         result.Response.Contacts.Count.Should().Be(1);
-        result.Response.Contacts[0].UserId.Should().Be(_user2.Id);
+        result.Response.Contacts[0].UserId.Should().Be(contact.Response.UserId);
     }
         
     public bool Seed()
