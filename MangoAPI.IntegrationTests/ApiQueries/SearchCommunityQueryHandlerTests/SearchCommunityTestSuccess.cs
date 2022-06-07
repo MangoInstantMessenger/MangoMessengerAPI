c52b003f60db6dde3fdfa444eb3885d869023659
Khachatur Khachatryan <xachulxx@gmail.com>
2022-06-07 16:39:21 +0300

    feature: implemented integration tests for api/chats/searches GET

--- MangoAPI.IntegrationTests/ApiQueries/SearchCommunityQueryHandlerTests/SearchCommunityTestSuccess.cs
+++ MangoAPI.IntegrationTests/ApiQueries/SearchCommunityQueryHandlerTests/SearchCommunityTestSuccess.cs
@@ -1,87 +1,35 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MangoAPI.BusinessLogic.ApiQueries.Communities;
-using MangoAPI.BusinessLogic.Responses;
 using MangoAPI.Domain.Constants;
-using MangoAPI.Domain.Entities;
-using MangoAPI.Domain.Enums;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
+using MangoAPI.IntegrationTests.Helpers;
 using Xunit;
 
 namespace MangoAPI.IntegrationTests.ApiQueries.SearchCommunityQueryHandlerTests;
 
-public class SearchCommunityTestSuccess : ITestable<SearchCommunityQuery, SearchCommunityResponse>
+public class SearchCommunityTestSuccess : IntegrationTestBase
 {
-    private readonly MangoDbFixture _mangoDbFixture = new();
     private readonly Assert<SearchCommunityResponse> _assert = new();
 
     [Fact]
     public async Task SearchCommunityTest_Success()
     {
-        Seed();
+        var user =
+            await MangoModule.RequestAsync(CommandHelper.RegisterPetroCommand(), CancellationToken.None);
+        var chat = await MangoModule.RequestAsync(
+                request: CommandHelper.CreateExtremeCodeMainChatCommand(user.Response.UserId), 
+                cancellationToken: CancellationToken.None);
         var query = new SearchCommunityQuery
         {
-            UserId = SeedDataConstants.RazumovskyId,
+            UserId = user.Response.UserId,
             DisplayName = "Extreme"
         };
-        var handler = CreateHandler();
 
-        var result = await handler.Handle(query, CancellationToken.None);
+        var result = await MangoModule.RequestAsync(query, CancellationToken.None);
             
         _assert.Pass(result);
         result.Response.Chats.Count.Should().Be(1);
-        result.Response.Chats[0].ChatId.Should().Be(SeedDataConstants.ExtremeCodeMainId);
+        result.Response.Chats[0].ChatId.Should().Be(chat.Response.ChatId);
     }
-        
-    public bool Seed()
-    {
-        _mangoDbFixture.Context.Users.Add(_user);
-        _mangoDbFixture.Context.Chats.Add(_chat);
-
-        _mangoDbFixture.Context.SaveChanges();
-
-        _mangoDbFixture.Context.Entry(_user).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_chat).State = EntityState.Detached;
-
-        return true;
-    }
-
-    public IRequestHandler<SearchCommunityQuery, Result<SearchCommunityResponse>> CreateHandler()
-    {
-        var responseFactory = new ResponseFactory<SearchCommunityResponse>();
-        var blobSettings = MockedObjects.GetBlobServiceSettingsMock();
-        var handler = new SearchCommunityQueryHandler(_mangoDbFixture.Context, responseFactory, blobSettings);
-        return handler;
-    }
-        
-    private readonly ChatEntity _chat = new()
-    {
-        Id = SeedDataConstants.ExtremeCodeMainId,
-        Title = "Extreme Code Main",
-        CommunityType = (int) CommunityType.PublicChannel,
-        Description = "Extreme Code Main Public Group",
-        CreatedAt = new DateTime(2020, 2, 4),
-        MembersCount = 4,
-        Image = "extreme_code_main.jpg",
-        UpdatedAt = DateTime.UtcNow,
-        LastMessageAuthor = "Amelit",
-        LastMessageText = "TypeScript The Best",
-        LastMessageTime = DateTime.Parse("2:32 PM")
-    };
-
-    private readonly UserEntity _user = new()
-    {
-        DisplayName = "razumovsky r",
-        Bio = "11011 y.o Dotnet Developer from $\"{cityName}\"",
-        Id = SeedDataConstants.RazumovskyId,
-        UserName = "razumovsky_r",
-        Email = "kolosovp95@gmail.com",
-        NormalizedEmail = "KOLOSOVP94@GMAIL.COM",
-        EmailConfirmed = true,
-        PhoneNumberConfirmed = true,
-        Image = "razumovsky_picture.jpg"
-    };
 }
