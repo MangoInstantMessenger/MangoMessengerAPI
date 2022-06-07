282397c7f7bdc119b1e1e5e7ac455515cf500a2e
Khachatur Khachatryan <xachulxx@gmail.com>
2022-06-07 15:26:02 +0300

    feature: implemented integration tests for api/chats GET

--- MangoAPI.IntegrationTests/ApiQueries/GetContactsQueryHandlerTests/GetContactsTestSuccess.cs
+++ MangoAPI.IntegrationTests/ApiQueries/GetContactsQueryHandlerTests/GetContactsTestSuccess.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
-using MangoAPI.BusinessLogic;
 using MangoAPI.BusinessLogic.ApiQueries.Contacts;
-using MangoAPI.BusinessLogic.Responses;
-using MangoAPI.Domain.Constants;
-using MangoAPI.Domain.Entities;
 using MangoAPI.IntegrationTests.Helpers;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace MangoAPI.IntegrationTests.ApiQueries.GetContactsQueryHandlerTests;
 
 public class GetContactsTestSuccess : IntegrationTestBase
 {
-    private readonly MangoDbFixture _mangoDbFixture = new();
     private readonly Assert<GetContactsResponse> _assert = new();
 
     [Fact]
@@ -42,61 +34,4 @@ public class GetContactsTestSuccess : IntegrationTestBase
         result.Response.Contacts.Count.Should().Be(1);
         result.Response.Contacts[0].UserId.Should().Be(contact.Response.UserId);
     }
-        
-    public bool Seed()
-    {
-        _mangoDbFixture.Context.Users.Add(_user1);
-        _mangoDbFixture.Context.Users.Add(_user2);
-        _mangoDbFixture.Context.UserContacts.Add(_contact);
-
-        _mangoDbFixture.Context.SaveChanges();
-
-        _mangoDbFixture.Context.Entry(_user1).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_user2).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_contact).State = EntityState.Detached;
-
-        return true;
-    }
-
-    public IRequestHandler<GetContactsQuery, Result<GetContactsResponse>> CreateHandler()
-    {
-        var responseFactory = new ResponseFactory<GetContactsResponse>();
-        var blobSettings = MockedObjects.GetBlobServiceSettingsMock();
-        var handler = new GetContactsQueryHandler(_mangoDbFixture.Context, responseFactory, blobSettings);
-        return handler;
-    }
-        
-    private readonly UserEntity _user1 = new()
-    {
-        DisplayName = "Khachatur Khachatryan",
-        Bio = "13 y. o. | C# pozer, Hearts Of Iron IV noob",
-        Id = SeedDataConstants.KhachaturId,
-        UserName = "KHACHATUR228",
-        Email = "xachulxx@gmail.com",
-        NormalizedEmail = "XACHULXX@GMAIL.COM",
-        EmailConfirmed = true,
-        PhoneNumberConfirmed = true,
-        Image = "khachatur_picture.jpg",
-    };
-
-    private readonly UserEntity _user2 = new()
-    {
-        DisplayName = "razumovsky r",
-        Bio = "11011 y.o Dotnet Developer from $\"{cityName}\"",
-        Id = SeedDataConstants.RazumovskyId,
-        UserName = "razumovsky_r",
-        Email = "kolosovp95@gmail.com",
-        NormalizedEmail = "KOLOSOVP94@GMAIL.COM",
-        EmailConfirmed = true,
-        PhoneNumberConfirmed = true,
-        Image = "razumovsky_picture.jpg"
-    };
-
-    private readonly UserContactEntity _contact = new()
-    {
-        Id = Guid.NewGuid(),
-        UserId = SeedDataConstants.KhachaturId,
-        ContactId = SeedDataConstants.RazumovskyId,
-        CreatedAt = DateTime.UtcNow
-    };
 }
--- MangoAPI.IntegrationTests/ApiQueries/GetCurrentUserChatsQueryHandlerTests/GetCurrentUserChatsTestSuccess.cs
+++ MangoAPI.IntegrationTests/ApiQueries/GetCurrentUserChatsQueryHandlerTests/GetCurrentUserChatsTestSuccess.cs
@@ -1,94 +1,33 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MangoAPI.BusinessLogic.ApiQueries.Communities;
-using MangoAPI.BusinessLogic.Responses;
-using MangoAPI.Domain.Constants;
-using MangoAPI.Domain.Entities;
-using MangoAPI.Domain.Enums;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
+using MangoAPI.IntegrationTests.Helpers;
 using Xunit;
 
 namespace MangoAPI.IntegrationTests.ApiQueries.GetCurrentUserChatsQueryHandlerTests;
 
-public class GetCurrentUserChatsTestSuccess : ITestable<GetCurrentUserChatsQuery, GetCurrentUserChatsResponse>
+public class GetCurrentUserChatsTestSuccess : IntegrationTestBase
 {
-    private readonly MangoDbFixture _mangoDbFixture = new();
     private readonly Assert<GetCurrentUserChatsResponse> _assert = new();
 
     [Fact]
     public async Task GetCurrentUserChatsTest_Success()
     {
-        Seed();
-        var handler = CreateHandler();
+        var user = await MangoModule.RequestAsync(
+            request: CommandHelper.RegisterPetroCommand(),
+            cancellationToken: CancellationToken.None);
+        var chat = await MangoModule.RequestAsync(
+            request: CommandHelper.CreateExtremeCodeMainChatCommand(user.Response.UserId),
+            cancellationToken: CancellationToken.None);
         var query = new GetCurrentUserChatsQuery
         {
-            UserId = SeedDataConstants.RazumovskyId
+            UserId = user.Response.UserId
         };
 
-        var result = await handler.Handle(query, CancellationToken.None);
+        var result = await MangoModule.RequestAsync(query, CancellationToken.None);
             
         _assert.Pass(result);
         result.Response.Chats.Count.Should().Be(1);
-    } 
-        
-    public bool Seed()
-    {
-        _mangoDbFixture.Context.Users.Add(_user);
-        _mangoDbFixture.Context.Chats.Add(_chat);
-        _mangoDbFixture.Context.UserChats.Add(_userChatEntity);
-
-        _mangoDbFixture.Context.SaveChanges();
-
-        _mangoDbFixture.Context.Entry(_user).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_chat).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_userChatEntity).State = EntityState.Detached;
-
-        return true;
     }
-
-    public IRequestHandler<GetCurrentUserChatsQuery, Result<GetCurrentUserChatsResponse>> CreateHandler()
-    {
-        var responseFactory = new ResponseFactory<GetCurrentUserChatsResponse>();
-        var blobSettings = MockedObjects.GetBlobServiceSettingsMock();
-        var handler = new GetCurrentUserChatsQueryHandler(_mangoDbFixture.Context, responseFactory, blobSettings);
-        return handler;
-    }
-        
-    private readonly ChatEntity _chat = new()
-    {
-        Id = SeedDataConstants.ExtremeCodeMainId,
-        Title = "Extreme Code Main",
-        CommunityType = (int) CommunityType.PublicChannel,
-        Description = "Extreme Code Main Public Group",
-        CreatedAt = new DateTime(2020, 2, 4),
-        MembersCount = 4,
-        Image = "extreme_code_main.jpg",
-        UpdatedAt = DateTime.UtcNow,
-        LastMessageAuthor = "Amelit",
-        LastMessageText = "TypeScript The Best",
-        LastMessageTime = DateTime.Parse("2:32 PM")
-    };
-
-    private readonly UserEntity _user = new()
-    {
-        DisplayName = "razumovsky r",
-        Bio = "11011 y.o Dotnet Developer from $\"{cityName}\"",
-        Id = SeedDataConstants.RazumovskyId,
-        UserName = "razumovsky_r",
-        Email = "kolosovp95@gmail.com",
-        NormalizedEmail = "KOLOSOVP94@GMAIL.COM",
-        EmailConfirmed = true,
-        PhoneNumberConfirmed = true,
-        Image = "razumovsky_picture.jpg"
-    };
-
-    private readonly UserChatEntity _userChatEntity = new()
-    {
-        UserId = SeedDataConstants.RazumovskyId,
-        ChatId = SeedDataConstants.ExtremeCodeMainId,
-        RoleId = (int) UserRole.Admin,
-    };
 }
