cd5100094eaa2ce2a0170c5f9d4fae1c32c16462
Khachatur Khachatryan <xachulxx@gmail.com>
2022-06-07 15:31:07 +0300

    feature: implemented integration tests for api/cng-key-exchange-requests GET

--- MangoAPI.IntegrationTests/ApiQueries/GetKeyExchangeRequestsQueryHandlerTests/GetKeyExchangeRequestsTestSuccess.cs
+++ MangoAPI.IntegrationTests/ApiQueries/GetKeyExchangeRequestsQueryHandlerTests/GetKeyExchangeRequestsTestSuccess.cs
@@ -1,91 +1,36 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MangoAPI.BusinessLogic.ApiQueries.CngKeyExchange;
-using MangoAPI.BusinessLogic.Responses;
-using MangoAPI.Domain.Constants;
-using MangoAPI.Domain.Entities;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
+using MangoAPI.IntegrationTests.Helpers;
 using Xunit;
 
 namespace MangoAPI.IntegrationTests.ApiQueries.GetKeyExchangeRequestsQueryHandlerTests;
 
-public class GetKeyExchangeRequestsTestSuccess : ITestable<CngGetKeyExchangeRequestsQuery, CngGetKeyExchangeResponse>
+public class GetKeyExchangeRequestsTestSuccess : IntegrationTestBase
 {
-    private readonly MangoDbFixture _mangoDbFixture = new();
     private readonly Assert<CngGetKeyExchangeResponse> _assert = new ();
 
     [Fact]
     public async Task GetKeyExchangeRequestsTest_Success()
     {
-        Seed();
+        var sender = 
+            await MangoModule.RequestAsync(CommandHelper.RegisterKhachaturCommand(), CancellationToken.None);
+        var requestedUser = 
+            await MangoModule.RequestAsync(CommandHelper.RegisterPetroCommand(), CancellationToken.None);
+        var keyExchangeRequest =
+            await MangoModule.RequestAsync(
+                CommandHelper.CreateCngKeyExchangeCommand(sender.Response.UserId, requestedUser.Response.UserId),
+                CancellationToken.None);
         var query = new CngGetKeyExchangeRequestsQuery
         {
-            UserId = SeedDataConstants.AmelitId
+            UserId = requestedUser.Response.UserId
         };
-        var handler = CreateHandler();
 
-        var result = await handler.Handle(query, CancellationToken.None);
+        var result = await MangoModule.RequestAsync(query, CancellationToken.None);
             
         _assert.Pass(result);
         result.Response.KeyExchangeRequests.Count.Should().Be(1);
-        result.Response.KeyExchangeRequests[0].RequestId.Should().Be(_cngKeyExchangeRequest.Id);
+        result.Response.KeyExchangeRequests[0].RequestId.Should().Be(keyExchangeRequest.Response.RequestId);
     }
-        
-    public bool Seed()
-    {
-        _mangoDbFixture.Context.AddRange(_sender, _receiver);
-        _mangoDbFixture.Context.CngKeyExchangeRequests.Add(_cngKeyExchangeRequest);
-
-        _mangoDbFixture.Context.SaveChanges();
-            
-        _mangoDbFixture.Context.Entry(_sender).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_receiver).State = EntityState.Detached;
-        _mangoDbFixture.Context.Entry(_cngKeyExchangeRequest).State = EntityState.Detached;
-            
-        return true;
-    }
-
-    public IRequestHandler<CngGetKeyExchangeRequestsQuery, Result<CngGetKeyExchangeResponse>> CreateHandler()
-    {
-        var responseFactory = new ResponseFactory<CngGetKeyExchangeResponse>();
-        var handler = new CngGetKeyExchangeRequestsQueryHandler(_mangoDbFixture.Context, responseFactory);
-        return handler;
-    }
-        
-    private readonly UserEntity _sender = new()
-    {
-        DisplayName = "razumovsky r",
-        Bio = "11011 y.o Dotnet Developer from $\"{cityName}\"",
-        Id = SeedDataConstants.RazumovskyId,
-        UserName = "razumovsky_r",
-        Email = "kolosovp95@gmail.com",
-        NormalizedEmail = "KOLOSOVP94@GMAIL.COM",
-        EmailConfirmed = true,
-        PhoneNumberConfirmed = true,
-        Image = "razumovsky_picture.jpg"
-    };
-
-    private readonly UserEntity _receiver = new()
-    {
-        DisplayName = "Amelit",
-        Bio = "Дипломат",
-        Id = SeedDataConstants.AmelitId,
-        UserName = "TheMoonlightSonata",
-        Email = "amelit@gmail.com",
-        NormalizedEmail = "AMELIT@GMAIL.COM",
-        EmailConfirmed = true,
-        PhoneNumberConfirmed = true,
-        Image = "amelit_picture.jpg"
-    };
-        
-    private readonly CngKeyExchangeRequestEntity _cngKeyExchangeRequest = new()
-    {
-        Id = Guid.NewGuid(),
-        SenderId = SeedDataConstants.RazumovskyId,
-        SenderPublicKey = "Public Key",
-        UserId = SeedDataConstants.AmelitId
-    };
 }
