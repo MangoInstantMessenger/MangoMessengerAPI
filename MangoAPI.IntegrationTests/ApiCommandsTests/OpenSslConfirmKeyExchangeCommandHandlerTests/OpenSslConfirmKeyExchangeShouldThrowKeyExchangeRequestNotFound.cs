c131f020bb601bd692cf6dce264241fe9796ef1e
Khachatur Khachatryan <xachulxx@gmail.com>
2022-06-10 23:01:37 +0300

    feature: implemented integration tests for api/openssl-key-exchange-requests/{requestId:guid} PUT

---
+++ MangoAPI.IntegrationTests/ApiCommandsTests/OpenSslConfirmKeyExchangeCommandHandlerTests/OpenSslConfirmKeyExchangeShouldThrowKeyExchangeRequestNotFound.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MangoAPI.BusinessLogic.ApiCommands.OpenSslKeyExchange;
+using MangoAPI.BusinessLogic.Responses;
+using MangoAPI.Domain.Constants;
+using MangoAPI.IntegrationTests.Helpers;
+using Xunit;
+
+namespace MangoAPI.IntegrationTests.ApiCommandsTests.OpenSslConfirmKeyExchangeCommandHandlerTests;
+
+public class OpenSslConfirmKeyExchangeShouldThrowKeyExchangeRequestNotFound : IntegrationTestBase
+{
+    private readonly Assert<ResponseBase> _assert = new();
+    
+    [Fact]
+    public async Task OpenSslConfirmKeyExchange_ShouldThrowKeyExchangeRequestNotFound()
+    {
+        const string expectedMessage = ResponseMessageCodes.KeyExchangeRequestNotFound;
+        var expectedDetails = ResponseMessageCodes.ErrorDictionary[expectedMessage];
+        var requestedUser = 
+            await MangoModule.RequestAsync(CommandHelper.RegisterPetroCommand(), CancellationToken.None);
+        var command = new OpenSslConfirmKeyExchangeCommand(
+            RequestId: Guid.NewGuid(), 
+            UserId: requestedUser.Response.UserId,
+            ReceiverPublicKey: MangoFilesHelper.GetTestImage());
+
+        var response = await MangoModule.RequestAsync(command, CancellationToken.None);
+        
+        _assert.Fail(response, expectedMessage, expectedDetails);
+    }
+}
