b67e404d62f1e669aa96c7f34b02e69da8f7af69
Artem Sveshnikov <artyom@sveshnikov.work>
2022-06-18 16:06:37 +0300

    Changed waiting script and pipeline

--- build/docker/mssql-docker/wait-for-mssql.sh
+++ build/docker/mssql-docker/wait-for-mssql.sh
@@ -1,4 +1,4 @@
-COUNT_DB=$(docker exec ${CONTAINER_NAME} /opt/mssql-tools/bin/sqlcmd -l 30 -h-1 -V1 -U sa -P ${PASSWORD_DATABASE} -Q "SET NOCOUNT ON;SELECT COUNT(name) FROM master.sys.databases")
+COUNT_DB=$(sqlcmd -l 30 -h-1 -V1 -S localhost,1433 -U sa -P ${PASSWORD_DATABASE} -Q "SET NOCOUNT ON;SELECT COUNT(name) FROM master.sys.databases")
 
 echo $COUNT_DB
 
