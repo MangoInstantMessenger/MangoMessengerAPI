1e7e0ef02d801ca0e7b0d5809338ef947f354d09
Artem Sveshnikov <artyom@sveshnikov.work>
2022-06-18 15:57:51 +0300

    Changed waiting script

--- build/docker/mssql-docker/wait-for-mssql.sh
+++ build/docker/mssql-docker/wait-for-mssql.sh
@@ -1,7 +1,10 @@
 COUNT_DB=$(docker exec ${CONTAINER_NAME} /opt/mssql-tools/bin/sqlcmd -l 30 -h-1 -V1 -U sa -P ${PASSWORD_DATABASE} -Q "SET NOCOUNT ON;SELECT COUNT(name) FROM master.sys.databases")
 
+echo $COUNT_DB
+
 COUNT_DB="${COUNT_DB//[$'\t\r\n ']}"
 
+echo $COUNT_DB
 if [ "$COUNT_DB" -gt 0 ] ;
 then
 	echo "Database running inside container "${CONTAINER_NAME}" is ready to accept incoming connections"
