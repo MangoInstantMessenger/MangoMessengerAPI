a99eed5982fcf4c4886bb62de1a2e16db09086b3
Artem Sveshnikov <artyom@sveshnikov.work>
2022-06-18 18:18:24 +0300

    Removed debuggin code in waiting script

--- build/docker/mssql-docker/wait-for-mssql.sh
+++ build/docker/mssql-docker/wait-for-mssql.sh
@@ -1,10 +1,7 @@
 COUNT_DB=$(sqlcmd -l 100 -h-1 -V1 -S localhost,1433 -U sa -P ${PASSWORD_DATABASE} -Q "SET NOCOUNT ON;SELECT COUNT(name) FROM master.sys.databases")
 
-echo $COUNT_DB
-
 COUNT_DB="${COUNT_DB//[$'\t\r\n ']}"
 
-echo $COUNT_DB
 if [ "$COUNT_DB" -gt 0 ] ;
 then
 	echo "Database running inside container "${CONTAINER_NAME}" is ready to accept incoming connections"
