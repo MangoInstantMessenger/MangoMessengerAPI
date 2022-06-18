f3a363d9b1e3e98c3596b158df16015832c790a0
Artem Sveshnikov <artyom@sveshnikov.work>
2022-06-18 15:49:11 +0300

    Installing sqlcmd

--- build/docker/mssql-docker/wait-for-mssql.sh
+++ build/docker/mssql-docker/wait-for-mssql.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-COUNT_DB=$(docker exec ${CONTAINER_NAME} /opt/mssql-tools/bin/sqlcmd -l 30 -h-1 -V1 -U sa -P ${PASSWORD_DATABASE} -Q "SET NOCOUNT ON;SELECT COUNT(name) FROM master.sys.databases")
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo apt-get update 
+sudo apt-get install mssql-tools unixodbc-dev
+
+COUNT_DB=$(/opt/mssql-tools/bin/sqlcmd -l 30 -h-1 -V1 -S localhost,1433 -U sa -P ${PASSWORD_DATABASE} -Q "SET NOCOUNT ON;SELECT COUNT(name) FROM master.sys.databases")
 
 COUNT_DB="${COUNT_DB//[$'\t\r\n ']}"
+
+echo $COUNT_DB
 
 if [ "$COUNT_DB" -gt 0 ] ;
 then
