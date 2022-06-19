30073890dd5dca6d4d710edc25057d28ea062b52
Khachatur Khachatryan <xachulxx@gmail.com>
2022-06-19 13:47:50 +0300

    featue: implemented emoji

--- MangoAPI.Client/src/app/components/chats/chats.component.ts
+++ MangoAPI.Client/src/app/components/chats/chats.component.ts
@@ -10,9 +10,16 @@ export class ChatsComponent implements OnInit {
   constructor() {
   }
 
+  public messageText: string = '';
+
   ngOnInit(): void {
     let chatMessages = document.getElementById('chatMessages');
     chatMessages!.scrollTop = chatMessages!.scrollHeight;
   }
 
+  onEmojiClick(event: Event): void {
+    let button = event.currentTarget as HTMLButtonElement;
+    let emoji = button.innerText;
+    this.messageText += emoji;
+  }
 }
