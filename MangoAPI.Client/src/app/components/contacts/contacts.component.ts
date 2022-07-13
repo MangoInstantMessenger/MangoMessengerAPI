dc5601e94babbfae1f97a476556c3a6f9c9f7aa5
Khachatur Khachatryan <xachulxx@gmail.com>
2022-07-14 00:37:05 +0300

    work in progress

--- MangoAPI.Client/src/app/components/contacts/contacts.component.ts
+++ MangoAPI.Client/src/app/components/contacts/contacts.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import {ContactsService} from "../../services/api/contacts.service";
 import {ErrorNotificationService} from "../../services/messenger/error-notification.service";
 import {Contact} from "../../types/models/Contact";
+import {UsersService} from "../../services/api/users.service";
+import {TokensService} from "../../services/messenger/tokens.service";
+import {formatDate} from "@angular/common";
+import {User} from "../../types/models/User";
 
 @Component({
   selector: 'app-contacts',
@@ -10,22 +14,52 @@ import {Contact} from "../../types/models/Contact";
 export class ContactsComponent {
 
   constructor(private _contactsService: ContactsService,
-              private _errorNotificationService: ErrorNotificationService) {
+              private _errorNotificationService: ErrorNotificationService,
+              private _usersService: UsersService,
+              private _tokensService: TokensService) {
   }
 
   public contacts: Contact[] = [];
-  public activeContact: Contact = {
-    userId: '',
-    displayName: '',
-    bio: '',
-    address: '',
-    isContact: false,
-    pictureUrl: ''
+  public activeContact: User = {
+    userId:  '',
+    firstName:  '',
+    lastName:  '',
+    displayName:  '',
+    birthdayDate:  '',
+    email:  '',
+    website:  '',
+    username:  '',
+    bio:  '',
+    address:  '',
+    facebook:  '',
+    twitter:  '',
+    instagram:  '',
+    linkedIn:  '',
+    publicKey:  0,
+    pictureUrl:  '',
   };
+  public currentUserId: string = this._tokensService.getTokens()?.userId as string;
   public activeContactUserId: string = '';
 
   ngOnInit(): void {
-    this.getUsersContacts();
+    this._usersService.getUserById(this.currentUserId).subscribe({
+      next: response => {
+        let user = response.user;
+        this.getUsersContacts();
+        this.activeContactUserId = user.userId;
+        this.activeContact = {
+          userId: user.userId,
+          displayName: user.displayName,
+          bio: user.bio,
+          address: user.address,
+          pictureUrl: user.pictureUrl
+        };
+      },
+      error: error => {
+        this._errorNotificationService.notifyOnError(error);
+      }
+    })
+
   }
 
   getUsersContacts(): void {
@@ -37,5 +71,9 @@ export class ContactsComponent {
         this._errorNotificationService.notifyOnError(error);
       }
     });
+  }
+
+  toDateParse(date: string): string {
+    return formatDate(date, "dd.MM.yyyy", "en-US");
   }
 }
