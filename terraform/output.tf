fdcdf2294649657346a282ee34210cb4fc350935
kolosovpetro <kolosovp94@gmail.com>
2022-10-16 20:48:00 +0200

    infrastructure applied

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg_messenger.name
+    value = azurerm_resource_group.rg_messenger.name
 }
 
-output "azurerm_postgresql_flexible_server" {
-  value = azurerm_postgresql_flexible_server.default.name
+output "azurerm_sql_server" {
+    value = azurerm_mssql_server.mango_sql_server.name
 }
 
-output "postgresql_flexible_server_database_name" {
-  value = azurerm_postgresql_flexible_server_database.default.name
+output "azurerm_sql_database_name" {
+    value = azurerm_mssql_database.mango_dev_db.name
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,52 +1,47 @@
 variable "resource_group_location" {
-  default     = "westus"
-  description = "Location of the resource group."
+    default     = "westus"
+    description = "Location of the resource group."
 }
 
 variable "resource_group_name" {
-  default     = "rg-messenger-d03"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+    default     = "rg-messenger-d03"
+    description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
 variable "storage_account_name" {
-  default     = "messengerstor21"
-  description = "Messenger storage account name"
+    default     = "mangostoraged01"
+    description = "Messenger storage account name"
 }
 
 variable "storage_account_tier" {
-  default     = "Standard"
-  description = "Messenger storage account tier"
+    default     = "Standard"
+    description = "Messenger storage account tier"
 }
 
 variable "storage_account_replication" {
-  default     = "GRS"
-  description = "Messenger storage account replication strategy"
+    default     = "GRS"
+    description = "Messenger storage account replication strategy"
 }
 
 variable "storage_container_name" {
-  default     = "cont-messenger"
-  description = "Messenger storage container name"
+    default     = "cont-messenger"
+    description = "Messenger storage container name"
 }
 
-variable "postgres_server_name" {
-  default     = "psql-messenger-d01"
-  description = "Messenger postgres server name"
+variable "sql_server_name" {
+    default     = "mango-sql-db-d01"
+    description = "Messenger sql server name"
 }
 
 #export TF_VAR_username=(the username)
 #export TF_VAR_password=(the password)
 
-variable "postgres_admin_username" {
-  type        = string
-  description = "Postgres admin username"
+variable "sql_admin_username" {
+    type        = string
+    description = "Sql admin username"
 }
 
-variable "postgres_admin_password" {
-  type        = string
-  description = "Postgres admin password"
-}
-
-variable "name_prefix" {
-  default     = "postgresqlfs"
-  description = "Prefix of the resource name."
+variable "sql_admin_password" {
+    type        = string
+    description = "Sql admin password"
 }
