13e4a7553150906f3476224c5caa3fe3f8f0ccc6
kolosovpetro <kolosovp94@gmail.com>
2022-11-03 22:12:02 +0100

    weak password test removed

--- MangoAPI.IntegrationTests/ApiCommandsTests/ChangePasswordCommandHandlerTests/ChangePasswordValidatorTestShouldThrowNewPasswordErrors.cs
+++ MangoAPI.IntegrationTests/ApiCommandsTests/ChangePasswordCommandHandlerTests/ChangePasswordValidatorTestShouldThrowNewPasswordErrors.cs
@@ -24,22 +24,4 @@ public class ChangePasswordValidatorTestShouldThrowNewPasswordErrors
         result.Errors.Count.Should().Be(1);
         result.Errors[0].ErrorMessage.Should().Be("New password and repeat password should be same.");
     }
-
-    [Fact]
-    public void ChangePasswordValidatorTest_ShouldThrowWeakPassword()
-    {
-        var validator = new ChangePasswordCommandValidator();
-        var command = new ChangePasswordCommand(
-            UserId: Guid.NewGuid(),
-            CurrentPassword: "Bm3-`dPRv-/w#3)cw^97",
-            NewPassword: "12345678",
-            RepeatNewPassword: "12345678");
-
-        var result = validator.TestValidate(command);
-
-        result.ShouldHaveValidationErrorFor(x => x.NewPassword);
-        result.Errors.Count.Should().Be(1);
-        result.Errors[0].ErrorMessage.Should().Be("Password must be at least 8 characters with: " +
-                                                  "1 uppercase, 1 lowercase, 1 digit, 1 symbol.");
-    }
 }
