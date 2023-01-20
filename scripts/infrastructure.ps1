8e2d5444f58424948e3543d2e40beeb4bb9ab676
Егор Кравченко <kravchenko13138@gmail.com>
2023-01-20 23:59:09 +0200

    update script

--- scripts/infrastructure.ps1
+++ scripts/infrastructure.ps1
@@ -1,10 +1,10 @@
-$resourceGroupName = "mango-rg"
+$rgName = "mango-rg"
+$servicePlanName = "mango-appservice-plan"
 $webAppName = "mangomessenger"
-$appServicePlanName = "mango-appservice-plan"
-$storageAccountName = "mangostorageaccounts"
+$storName = "mangostorageaccounts"
 $skuPlanName = "FREE"
 $skuStorageName = "Standard_LRS"
-$storageContainerName = "mangocontainer"
+$contName = "mangomessengercontainer"
 $keyVaultName = "mangokeyvaults"
 $sqlServerName = "mangosqlserver"
 $mangoDb = "mangodb"
@@ -16,25 +16,25 @@ $adminPassword = "Admin123"
 
 
 Write-Output "Creating $resourceGroup in "$location"..."
-az group create --name $resourceGroupName --location "$location"
+az group create --name $rgName --location "$location"
 
 Write-Output "Creating $appServicePlanName"
-az appservice plan create --name $appServicePlanName --resource-group $resourceGroupName --sku $skuPlanName
+az appservice plan create --name $servicePlanName --resource-group $rgName --sku $skuPlanName
 
 Write-Output "Creating $webAppName"
-az webapp create --name $webAppName --resource-group $resourceGroupName --plan $appServicePlanName
+az webapp create --name $webAppName --resource-group $rgName --plan $servicePlanName
 
-Write-Output "Creating $storageAccountName"
-az storage account create --name $storageAccountName --resource-group $resourceGroupName --location $location --sku $skuStorageName
+Write-Output "Creating $storName"
+az storage account create --name $storName --resource-group $rgName --location $location --sku $skuStorageName
 
-Write-Output "Creating $storageContainerName"
-az storage container create --name $storageContainerName --account-name $storageAccountName --fail-on-exist
+Write-Output "Creating $contName"
+az storage container create --name $contName --account-name $storName --public-access "off"
 
 Write-Output "Creating $keyVaultName"
-az keyvault create --name $keyVaultName --resource-group $resourceGroupName --location $location
+az keyvault create --name $keyVaultName --resource-group $rgName --location $location
 
 Write-Output "Creating $sqlServerName"
-az sql server create --name $sqlServerName --resource-group $resourceGroupName --location $location --admin-user $adminUser --admin-password $adminPassword
+az sql server create --name $sqlServerName --resource-group $rgName --location $location --admin-user $adminUser --admin-password $adminPassword
 
 Write-Output "Creating $mangoDb"
 az sql db create --name $mangoDb --resource-group $resourceGroupName --server $sqlServerName
