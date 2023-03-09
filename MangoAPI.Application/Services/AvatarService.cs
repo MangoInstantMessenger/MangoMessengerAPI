a92db52680bc583ca0a7a7f862c2089ce70f66be
kami no me <kudryashov.kd@gmail.com>
2023-03-10 01:09:04 +0400

    add more anime pictures in avatar service

--- MangoAPI.Application/Services/AvatarService.cs
+++ MangoAPI.Application/Services/AvatarService.cs
@@ -7,7 +7,10 @@ namespace MangoAPI.Application.Services;
 public class AvatarService : IAvatarService
 {
     private readonly List<string> avatars = new() { "animetyanpic1.jpg", "animetyanpic2.jpg", "animetyanpic3.jpg", 
-        "animetyanpic4.jpg", "animetyanpic5.jpg", "animetyanpic6.jpg", "animetyanpic7.jpg", "animetyanpic8.jpg"}; 
+        "animetyanpic4.jpg", "animetyanpic5.jpg", "animetyanpic6.jpg", "animetyanpic7.jpg", "animetyanpic8.jpg",
+        "animetyanpic9.jpg", "animetyanpic10.jpg", "animetyanpic11.jpg", "animetyanpic12.jpg", "animetyanpic13.jpg",
+        "animetyanpic14.jpg", "animetyanpic15.jpg", "animetyanpic16.jpg", "animetyanpic17.jpg"
+    }; 
     
     public string GetRandomAvatar()
     {
