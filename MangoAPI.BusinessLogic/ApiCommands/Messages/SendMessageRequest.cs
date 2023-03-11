5689963c6edfa36c079e204b053a97fcc6de49c1
kolosovpetro <kolosovp94@gmail.com>
2023-03-11 16:39:55 +0100

    tested with postman

--- MangoAPI.BusinessLogic/ApiCommands/Messages/SendMessageRequest.cs
+++ MangoAPI.BusinessLogic/ApiCommands/Messages/SendMessageRequest.cs
@@ -5,44 +5,31 @@ using System.Text.Json.Serialization;
 
 namespace MangoAPI.BusinessLogic.ApiCommands.Messages;
 
-public record SendMessageRequest
+public class SendMessageRequest
 {
-    [JsonConstructor]
-    public SendMessageRequest(
-        string messageText,
-        Guid chatId,
-        string inReplayToAuthor,
-        string inReplayToText,
-        DateTime? createdAt,
-        Guid? messageId,
-        IFormFile attachment)
-    {
-        MessageText = messageText;
-        ChatId = chatId;
-        InReplayToAuthor = inReplayToAuthor;
-        InReplayToText = inReplayToText;
-        CreatedAt = createdAt;
-        MessageId = messageId;
-        Attachment = attachment;
-    }
-
     [DefaultValue("hello world")]
-    public string MessageText { get; }
+    [JsonPropertyName("messageText")]
+    public string MessageText { get; set; }
 
     [DefaultValue("a8747c37-c5ef-4a87-943c-3ee3ae0a2871")]
-    public Guid ChatId { get; }
+    [JsonPropertyName("chatId")]
+    public Guid ChatId { get; set; }
 
     [DefaultValue("John Doe")]
-    public string InReplayToAuthor { get; }
+    [JsonPropertyName("inReplayToAuthor")]
+    public string InReplayToAuthor { get; set; }
 
     [DefaultValue("Hello world!")]
-    public string InReplayToText { get; }
+    [JsonPropertyName("inReplayToText")]
+    public string InReplayToText { get; set; }
 
     [DefaultValue("2021-08-01T00:00:00.0000000")]
-    public DateTime? CreatedAt { get; }
+    [JsonPropertyName("createdAt")]
+    public DateTime? CreatedAt { get; set; }
 
     [DefaultValue("f56ac722-a57b-411c-8306-c2e05fb1a8df")]
-    public Guid? MessageId { get; }
-    
-    public IFormFile Attachment { get; }
+    [JsonPropertyName("messageId")]
+    public Guid? MessageId { get; set; }
+
+    public IFormFile Attachment { get; set; }
 }
