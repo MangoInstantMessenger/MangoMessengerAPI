c05120b0e38062e9a5ab7f28a9d2ac4789d031b0
kami no me <kudryashov.kd@gmail.com>
2023-03-12 01:01:16 +0400

    add redirect if refresh token is invalid

--- MangoAPI.Client/src/app/interceptors/request-header.interceptor.ts
+++ MangoAPI.Client/src/app/interceptors/request-header.interceptor.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import {
   HttpRequest,
@@ -11,10 +12,16 @@ import { TokensService } from '../services/messenger/tokens.service';
 
 @Injectable()
 export class RequestHeaderInterceptor implements HttpInterceptor {
-  constructor(private tokensService: TokensService) {}
+  constructor(
+    private tokensService: TokensService,
+    private router: Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const accessToken = this.tokensService.getTokens()?.accessToken;
+
+    if (accessToken === undefined) {
+      this.router.navigateByUrl("login");
+    }
 
     const addHeaderRequest = request.clone({
       headers: new HttpHeaders({ Authorization: 'Bearer ' + accessToken })
