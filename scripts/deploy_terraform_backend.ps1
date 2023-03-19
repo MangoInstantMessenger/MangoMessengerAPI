f6c85e805dc3061804fff31f38875ae92137d9b7
kolosovpetro <kolosovp94@gmail.com>
2023-03-19 22:24:08 +0100

    terraform deploy stages

--- scripts/deploy_terraform_backend.ps1
+++ scripts/deploy_terraform_backend.ps1
@@ -24,6 +24,12 @@ $spName = "SpTfAzureDevops$( Get-Random 1000 )"
 
 #.\deploy_terraform_backend.ps1 -prefix "ado019"
 
+# AZURE_FOR_STUDENTS_TFSTATE_KOLOSOVP94
+#.\deploy_terraform_backend.ps1 -prefix "dev01" -subscriptionId "e3b8e7eb-628d-4a3c-80e5-b80bf1eab292"
+
 # AZURE_FOR_STUDENTS_TFSTATE_RZAUMOVSKEY
 #.\deploy_terraform_backend.ps1 -prefix "qa01" -subscriptionId "fab0735b-aac3-490e-ad20-68043a66483b"
+
+# OSDS_TFSTATE_PKOLOSOV
+#.\deploy_terraform_backend.ps1 -prefix "uat02" -subscriptionId "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
 
