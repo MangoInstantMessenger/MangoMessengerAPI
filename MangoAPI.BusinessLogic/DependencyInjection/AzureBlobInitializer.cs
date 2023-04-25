e80bdee41eb940e25bca538c2492850db046700d
kami no me <kudryashov.kd@gmail.com>
2023-04-25 22:10:38 +0400

    update dockerfile and docker compose

--- MangoAPI.BusinessLogic/DependencyInjection/AzureBlobInitializer.cs
+++ MangoAPI.BusinessLogic/DependencyInjection/AzureBlobInitializer.cs
@@ -1,12 +1,15 @@
 using MangoAPI.Application.Interfaces;
+using MangoAPI.Domain.Constants;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace MangoAPI.BusinessLogic.DependencyInjection;
 
 public static class AzureBlobInitializer
 {
     private const string SeedImagesFolder = "../../../../img/seed_images";
+    private const string SeedImagesFolderWithinDockerContainer = "img/seed_images";
 
     public static void InitializeAzureBlob(this IApplicationBuilder app)
     {
@@ -14,6 +17,16 @@ public static class AzureBlobInitializer
             .CreateScope();
 
         var blobService = serviceScope.ServiceProvider.GetService<IBlobService>();
+
+        var runningInContainerEnvValue = Environment.GetEnvironmentVariable(EnvironmentConstants.AspNetCoreRunningInContainer);
+
+        var result = bool.TryParse(runningInContainerEnvValue, out var isRunningInContainer);
+
+        if (result && isRunningInContainer)
+        {
+            blobService.UploadFolderToBlob(SeedImagesFolderWithinDockerContainer);
+            return;
+        }
 
         blobService.UploadFolderToBlob(SeedImagesFolder);
     }
